aae55d5d92978215e822ac7cfc1d0199c06a6f32
Omar Sinan <omaro.hashim@hotmail.com>
2021-02-02 11:24:41 +0300

    Redirect main page to new portfolio

--- assets/index.js
+++ assets/index.js
@@ -1,29 +1,33 @@
 // typed.js init
-var options = {
-    strings:["Hi.^1000","I'm Omar.^1000","I program random things.^2000"],
-    typeSpeed:40,
-    startDelay:500,
-    backSpeed:40,
-    backDelay:1000,
-    loop:true,
-}
-var typed = new Typed(".typed", options)
+// var options = {
+//     strings:["Hi.^1000","I'm Omar.^1000","I program random things.^2000"],
+//     typeSpeed:40,
+//     startDelay:500,
+//     backSpeed:40,
+//     backDelay:1000,
+//     loop:true,
+// }
+// var typed = new Typed(".typed", options)
 
-window.onload = function () {
-    // scroll to div on click of buttons
-    const homeLink = document.querySelector('.homeLink')
-    const projectsLink = document.querySelectorAll('.projectsLink')
-    const projectsTitle = document.getElementById('projectsTitle')
-    for (var i = 0; i < projectsLink.length; i++) {
-        projectsLink[i].addEventListener('click', function() {
-            skrollTop.scrollTo({
-                to: projectsTitle.offsetTop - 20
-            })
-        })
-    }
-    homeLink.addEventListener('click', function() {
-        skrollTop.scrollTo({
-            to: 0
-        })
-    })
+// window.onload = function () {
+//     // scroll to div on click of buttons
+//     const homeLink = document.querySelector('.homeLink')
+//     const projectsLink = document.querySelectorAll('.projectsLink')
+//     const projectsTitle = document.getElementById('projectsTitle')
+//     for (var i = 0; i < projectsLink.length; i++) {
+//         projectsLink[i].addEventListener('click', function() {
+//             skrollTop.scrollTo({
+//                 to: projectsTitle.offsetTop - 20
+//             })
+//         })
+//     }
+//     homeLink.addEventListener('click', function() {
+//         skrollTop.scrollTo({
+//             to: 0
+//         })
+//     })
+// }
+
+window.onload = function() {
+    window.location.replace("https://omarsinan.com");
 }
